113832756c4bc2035c17e8e9e770fb3b3c082445
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2024-01-28 18:31:40 -0500

    fix addProduct page

--- app/dashboard/products/add/page.tsx
+++ app/dashboard/products/add/page.tsx
@@ -4,22 +4,22 @@ const AddProductPage = () => {
   return (
     <div className={styles.container}>
       <form action="" className={styles.form}>
-        <input type="text" placeholder="title" name="title" required />
+        <input type="text" placeholder="Title" name="title" required />
         <select name="category" id="category">
           <option value="default">Choose a category</option>
           <option value="tv">TV</option>
           <option value="phone">Phone</option>
           <option value="computer">Computer</option>
         </select>
-        <input type="number" placeholder="price" name="price" />
-        <input type="number" placeholder="stock" name="stock" />
-        <input type="text" placeholder="color" name="color" />
-        <input type="text" placeholder="size" name="size" />
+        <input type="number" placeholder="Price" name="price" />
+        <input type="number" placeholder="Stock" name="stock" />
+        <input type="text" placeholder="Color" name="color" />
+        <input type="text" placeholder="Size" name="size" />
         <textarea
           name="description"
           id="description"
           placeholder="Description"
-          rows={16}
+          rows={4}
         />
         <div className={styles.buttonWrapper}>
           <button type="submit">Submit</button>
