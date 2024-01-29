dc97ac4ec05c522845a9fcfc9131d326d11e1503
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2024-01-28 19:12:47 -0500

    add login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,16 @@
+import styles from "../ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1 className={styles.title}>Login</h1>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
