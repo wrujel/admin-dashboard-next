c66c76cf33aa0b27c8ea054447cc6fc6290d9fe3
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2024-01-30 21:14:04 -0500

    add search and pagination for users

--- app/ui/dashboard/pagination/pagination.tsx
+++ app/ui/dashboard/pagination/pagination.tsx
@@ -1,12 +1,44 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 import styles from "./pagination.module.css";
 
-const Pagination = () => {
+const Pagination = ({ totalPages }: { totalPages: number }) => {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const params = new URLSearchParams(searchParams);
+  const { replace } = useRouter();
+
+  const page = params.get("page") || "1";
+  const hasPrev = parseInt(page) > 1;
+  const hasNext = parseInt(page) <= totalPages;
+
+  const handlePrev = () => {
+    params.set("page", (parseInt(page) - 1).toString());
+    replace(`${pathName}?${params.toString()}`);
+  };
+
+  const handleNext = () => {
+    params.set("page", (parseInt(page) + 1).toString());
+    replace(`${pathName}?${params.toString()}`);
+  };
+
   return (
     <div className={styles.container}>
-      <button className={styles.button} disabled>
+      <button
+        className={styles.button}
+        disabled={!hasPrev}
+        onClick={handlePrev}
+      >
         Previous
       </button>
-      <button className={styles.button}>Next</button>
+      <button
+        className={styles.button}
+        disabled={!hasNext}
+        onClick={handleNext}
+      >
+        Next
+      </button>
     </div>
   );
 };
--- app/ui/dashboard/search/search.tsx
+++ app/ui/dashboard/search/search.tsx
@@ -1,11 +1,40 @@
+"use client";
+
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }: { placeholder: string }) => {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const params = new URLSearchParams(searchParams);
+  const { replace } = useRouter();
+
+  const searchDebounced = useDebouncedCallback((search) => {
+    params.set("search", search);
+    replace(`${pathName}?${params.toString()}`);
+  }, 400);
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    if (query.length > 0) searchDebounced(query);
+    else {
+      searchDebounced.cancel();
+      params.delete("search");
+      replace(`${pathName}?${params.toString()}`);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <MdSearch />
-      <input type="text" placeholder={placeholder} className={styles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
